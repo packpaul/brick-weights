9a2eb33d399b09db6399c290b9e0ba0a9c5a47dd
Pavel Perminov <packpaul@mail.ru>
2016-11-23 00:17:38 +0300

    Initial structure

---
+++ src/main/java/com/pp/brickweights/BrickWeightSolver.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright - Unlicense (http://unlicense.org)
+ */
+package com.pp.brickweights;
+
+/**
+ * Class encapsulates algorithm for solving brick weights problem. Algorithm complexity should be less than O(n^2 / 2),
+ * where <i>n</i> is the depth of a brick row
+ * 
+ * @author Pavel Perminov (packpaul@mail.ru)
+ */
+public class BrickWeightSolver {
+    
+    /**
+     * Finds the weight pressure that is exposed to the brick in row {@code row} at position {@code pos} 
+     * @param row  row 0-based
+     * @param pos  position 0-based
+     * @return
+     */
+    public double weight(int row, int pos) {
+        // TODO:
+        return 0.0;
+    }
+ 
+}
---
+++ src/main/java/com/pp/brickweights/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2016 by Pavel Perminov (packpaul@mail.ru)
+ */
+package com.pp.brickweights;
+
+/**
+ * Class for demonstrating Brick weights solver 
+ * 
+ * @author Pavel Perminov (packpaul@mail.ru)
+ */
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        final int row = 322;
+        final int pos = 156;
+        
+        double weight = new BrickWeightSolver().weight(row, pos);
+        
+        System.out.println(
+                String.format("Weight pressure exposed to the brick in position (%d, %d) is %f", row, pos, weight));
+    }
+    
+}
